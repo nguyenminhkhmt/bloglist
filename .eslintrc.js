d5a4978391f27360e9fcd27a76a25775a289966c
Duc Minh Nguyen <nguyenminhkhmt@gmail.com>
2023-03-06 07:28:30 +0200

    Initial code FE

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,27 @@ module.exports = {
     'browser': true,
     'commonjs': true,
     'es2021': true,
-    'jest': true
+    'jest': true,
+    'es6': true,
+    'jest/globals': true,
+    'cypress/globals': true
   },
-  'extends': 'eslint:recommended',
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
   'overrides': [
   ],
   'parserOptions': {
-    'ecmaVersion': 'latest'
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
   },
+  'plugins': [
+    'react', 'jest', 'cypress'
+  ],
   'rules': {
     'indent': [
       'error',
@@ -36,6 +49,13 @@ module.exports = {
     'arrow-spacing': [
       'error', { 'before': true, 'after': true }
     ],
-    'no-console': 0
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off'
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
   }
 }
